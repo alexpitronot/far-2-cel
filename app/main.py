0c86fa52b9edaa7ecaa5784e32c061644edde83d
agorbach <agorbach@gmail.com>
2021-05-09 20:17:12 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return “Our first Kubernetes app"
+    return "Our first Kubernetes app"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0’)
+    app.run(host='0.0.0.0')
