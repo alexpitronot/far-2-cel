fa4139ed9758a02100456dfed370191b1d03b307
EC2 Default User <ec2-user@ip-172-31-26-242.us-west-2.compute.internal>
2021-05-25 15:16:26 +0000

    Add main1.py

--- app/main1.py
+++ app/main1.py
@@ -4,9 +4,9 @@ APP = Flask(__name__)
 
 @APP.route('/')
 def hello_world():
-    return 'Our first Kubernetes app new'
+    return 'Our first Kubernetes app'
 
 
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(host='0.0.0.0')
