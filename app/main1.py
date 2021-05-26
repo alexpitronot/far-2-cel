4eee48c3c24b2cc54d778f984537303411e4f6db
agorbach <agorbach@gmail.com>
2021-05-26 17:04:22 +0300

    Update main1.py

--- app/main1.py
+++ app/main1.py
@@ -1,12 +1,20 @@
 from flask import Flask
-APP = Flask(__name__)
 
+app = Flask(__name__)
 
-@APP.route('/')
-def hello_world():
-    return 'Our first Kubernetes app'
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!"
 
+@app.route("/")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
 
-
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
