79023ae7154bb69cc5d4322b6272ed95df986a49
agorbach <agorbach@gmail.com>
2021-06-22 16:19:27 +0300

    Update main1.py

--- app/main1.py
+++ app/main1.py
@@ -17,8 +17,8 @@ def index():
                 <input type="submit" value="Convert to Fahrenheit">
             </div>
             </form>"""
-        + """<div style="background-color: red;color: white; font-size: 2.6em; display: unset; padding: 6px;">Fahrenheit: """
-        + fahrenheit + "</div>"
+        + """<div style="background-color: red;color: white; font-size: 2.6em; display: unset; padding: 6px;">Fahrenheit: <b>"""
+        + fahrenheit + "</b></div>"
     )
 
 def fahrenheit_from(celsius):
